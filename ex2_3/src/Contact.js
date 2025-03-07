637ed297f0e2ae431f5d28f72a6cc2a4243eaec5
cee2tall <182031182+cee2tall@users.noreply.github.com>
2025-03-07 11:33:31 -0700

    ex2 ex3

---
+++ ex2_3/src/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Contact() {
+  return (
+    <div>
+      <p>Contact Us</p>
+    </div>
+  );
+}
+export default Contact;
---
+++ ex2_3/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>Welcome to the Home Page</p>
+    </div>
+  );
+}
+export default Home;
